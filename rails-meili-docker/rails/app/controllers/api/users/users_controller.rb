91086b217f688119904128b6a54c02967920802b
ndjndj <teq.ndj@gmail.com>
2024-01-12 13:04:07 +0900

    f

--- rails-meili-docker/rails/app/controllers/api/users/users_controller.rb
+++ rails-meili-docker/rails/app/controllers/api/users/users_controller.rb
@@ -40,13 +40,3 @@ class Api::Users::UsersController < ApplicationController
       params.require(:user).permit(:user_system_id, :name, :name_ruby, :url)
     end
 end
-
-
-# curl \
-#  -X POST http://localhost:3000/api/users \
-#  -d '{"user_system_id": "", "name": "post_name", "name_ruby": "post_name", "url": "test@example.com"}' \
-#  -H "Content-Type: application/json"
-
-# curl -X DELETE http://localhost:3000/api/users/1000070000 \
-
-# -H "Content-Type: application/json"
