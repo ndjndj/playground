5b8d6a9978b07fe78aa46604cd3a7917922a921a
ndjndj <teq.ndj@gmail.com>
2024-01-12 11:46:41 +0900

    f

---
+++ rails-meili-docker/rails/app/controllers/api/users/users_controller.rb
@@ -0,0 +1,36 @@
+class Api::Users::UsersController < ApplicationController
+end
+class Api::UsersController < ApplicationController
+
+  def index
+    query = params[:q]
+    query = "" if query.blank?
+
+    user = User.ms_search(query)
+
+    render json: user,
+           root: 'data',
+           adapter: :json
+  end
+
+  def create
+    user = User.create!(required_params)
+    render json: user,
+           root: 'data',
+           adapter: :json
+  end
+
+  def update
+    render json: {message: "update"}, status: :ok
+  end
+
+  def delete
+
+    render json: {message: "delete"}, status: :ok
+  end
+
+  private
+    def required_params
+      params.require(:user).permit(:name, :name_ruby, :url)
+    end
+end
