40a9a4c8a62e352848dd82261161e5c5dd53df96
ndjndj <teq.ndj@gmail.com>
2024-01-12 10:27:54 +0900

    f

--- rails-meili-docker/rails/app/controllers/api/users_controller.rb
+++ rails-meili-docker/rails/app/controllers/api/users_controller.rb
@@ -1,19 +1,19 @@
 class Api::UsersController < ApplicationController
 
   def index
-
+    render json: {message: "index"}, status: :ok
   end
 
   def create
-
+    render json: {message: "create"}, status: :ok
   end
 
   def update
-
+    render json: {message: "update"}, status: :ok
   end
 
   def delete
-
+    render json: {message: "delete"}, status: :ok
   end
-  
+
 end
