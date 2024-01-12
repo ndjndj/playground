60371a1b1d19070c14d11a0a001c2e2cfd8bea0d
ndjndj <teq.ndj@gmail.com>
2024-01-12 10:06:32 +0900

    f

--- rails-meili-docker/rails/config/initializers/meilisearch.rb
+++ rails-meili-docker/rails/config/initializers/meilisearch.rb
@@ -1,3 +1,4 @@
 MeiliSearch::Rails.configuration = {
-  meilisearch_url: ENV.fetch('MEILISEARCH_HOST', 'http://meilisearch:7700'), meilisearch_api_key: ENV.fetch('MEILISEARCH_API_KEY', 'meili-master-key')
+  meilisearch_url: ENV.fetch('MEILISEARCH_HOST', 'http://meilisearch:7700'),
+  meilisearch_api_key: ENV.fetch('MEILISEARCH_API_KEY', 'meili-master-key')
 }
